7db99383031ea26b2c827fa8b2d4d9936299d72d
Nicolas Parducci <nicolasparducci65@gmail.com>
2026-06-26 18:32:14 -0500

    Convertir en PWA instalable: iconos, manifest y service worker

---
+++ sw.js
@@ -0,0 +1,52 @@
+﻿const CACHE_NAME = "hidivo-gdm-v1";
+const ARCHIVOS_ESTATICOS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS_ESTATICOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(
+        nombres
+          .filter((nombre) => nombre !== CACHE_NAME)
+          .map((nombre) => caches.delete(nombre))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.hostname.includes("supabase.co")) {
+    return;
+  }
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).then((response) => {
+        if (response.ok && url.origin === self.location.origin) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        }
+        return response;
+      });
+    }).catch(() => fetch(event.request))
+  );
+});
